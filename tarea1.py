40d56f2876d5009b0ced12ad70cf80ed5b031824
Benjamín Molina <benjaminiquique@gmail.com>
2021-04-26 16:06:03 -0400

    feature: added log to empty string error

--- tarea1.py
+++ tarea1.py
@@ -11,9 +11,10 @@ inputString = input("Ingrese la cadena a comprimir: ")
 logging.info("Cadena ingresada: {string}".format(string=inputString))
 
 if "-" in inputString:
-    logging.error("La cadena ingresada contiene un \"-\"")
-    raise ValueError("Ingresaste el caracter especial reservado '-'")
+    logging.error("La cadena ingresada contiene un \"-\".")
+    raise ValueError("Ingresaste el caracter especial reservado '-'.")
 if len(inputString) == 0:
+    logging.error("Se ingresó una cadena vacía.")
     raise ValueError("Ingresaste una cadena vacía")
 
 compressedString = ""
@@ -31,7 +32,7 @@ for charIndex in range(len(inputString)):
         repetitions += 1
 
     if isNotFirstChar and (isDifferentChar or isLastChar):
-        logging.info("Se encontraron {repetitions} repeticiones del caracter '{character}''.".format(
+        logging.info("Se encontraron {repetitions} repeticiones del caracter '{character}'.'.".format(
             repetitions=repetitions, character=savedChar))
         if savedChar.isnumeric():
             savedChar = "{separator}{character}{separator}".format(
@@ -41,7 +42,7 @@ for charIndex in range(len(inputString)):
         repetitions = 1
 
     if (isDifferentChar and isLastChar):
-        logging.info("Se encontraron {repetitions} repeticiones del caracter '{character}''.".format(
+        logging.info("Se encontraron {repetitions} repeticiones del caracter '{character}'.'.".format(
             repetitions=repetitions, character=char))
         if char.isnumeric():
             char = "{separator}{character}{separator}".format(
@@ -56,6 +57,6 @@ logging.info("El largo de la cadena comprimida es: {len}.".format(
     len=len(compressedString)))
 result = compressedString if len(
     compressedString) < len(inputString) else inputString
-logging.info("La cadena devuelta es: {string}".format(string=result))
+logging.info("La cadena devuelta es: {string}.".format(string=result))
 
-logging.info("Fin del programa\n")
+logging.info("Fin del programa.\n")
